6fab5afd380dc1b4b19f306a24e3557012d18bc6
Fabiao Wang <it_master2003@hotmail.com>
2018-11-20 23:38:04 +0800

    Added filter for VS output

--- include/datafetch/IDataFetch.hpp
+++ include/datafetch/IDataFetch.hpp
@@ -2,9 +2,12 @@
 #define INTERFACE_DATA_FETCH_H
 
 #include <opencv2/opencv.hpp>
-#pragma comment(lib, "libvlc.lib")
-#pragma comment(lib, "libvlccore.lib")
-#pragma comment(lib, "opencv_world343d.lib")
+
+#ifdef _WIN64
+	#pragma comment(lib, "libvlc.lib")
+	#pragma comment(lib, "libvlccore.lib")
+	#pragma comment(lib, "opencv_world343d.lib")
+#endif
 
 class IDataFetch
 {
