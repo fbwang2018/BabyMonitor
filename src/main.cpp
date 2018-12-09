37ddba789c402e69ee02637e5699535e3b11053b
Fabiao Wang <it_master2003@hotmail.com>
2018-12-09 20:17:42 +0800

    Added time to break

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,15 @@ int main(int argc, char *argv[])
 		string img = babyMonitor.GetImage();
 
 		std::this_thread::sleep_for(300s);
+
+		auto t = chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+		struct tm ts;
+		localtime_s(&ts, &t);
+		if (ts.tm_hour == 6 && ts.tm_min == 30)
+		{
+			break;
+		}
 	}
 
 	return 0;
